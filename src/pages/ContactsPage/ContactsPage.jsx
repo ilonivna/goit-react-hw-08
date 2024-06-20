ca135e7afe89503164776fc45ab6da7a1ee3a160
ilonivna <147661751+ilonivna@users.noreply.github.com>
2024-06-20 11:09:24 +0300

    comment editor

--- src/pages/ContactsPage/ContactsPage.jsx
+++ src/pages/ContactsPage/ContactsPage.jsx
@@ -2,7 +2,7 @@ import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import DocumentTitle from "../../components/DocumentTitle/DocumentTitle";
 import { ContactList } from "../../components/ContactList/ContactList";
-import ContactEditor from "../../components/ContactEditor/ContactEditor";
+// import ContactEditor from "../../components/ContactEditor/ContactEditor";
 import { fetchContacts } from "../../redux/contacts/operations";
 import { selectLoading } from "../../redux/contacts/selectors";
 import Loader from "../../components/Loader/Loader";
